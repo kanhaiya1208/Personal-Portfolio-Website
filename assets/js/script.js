f8637280cdb68ca142ee67a96f4fc8657fc8fafd
Kanhaiya Lal <123859716+kanhaiya12083@users.noreply.github.com>
2024-02-07 21:55:21 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -88,6 +88,22 @@ async function fetchData(type = "skills") {
     const data = await response.json();
     return data;
 }
+
+function showSkills(skills) {
+    let skillsContainer = document.getElementById("skillsContainer");
+    let skillHTML = "";
+    skills.forEach(skill => {
+        skillHTML += `
+        <div class="bar">
+              <div class="info">
+                <img src=${skill.icon} alt="skill" />
+                <span>${skill.name}</span>
+              </div>
+            </div>`
+    });
+    skillsContainer.innerHTML = skillHTML;
+}
+
 fetchData().then(data => {
     showSkills(data);
 });
